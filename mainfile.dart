ba61c8a82747e7dab493210f2c725d8929c91183
melinaabella <41589124+melinaabella@users.noreply.github.com>
2019-04-20 17:25:54 -0700

    Create mainfile.dart

---
+++ mainfile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: 'WiCSE',
+      theme: ThemeData(
+      primaryColor: Colors.indigo[100],
+      accentColor: Colors.indigo[200],
+      //font
+      fontFamily: 'Roboto',
+      //defining default TextTheme
+      //text style for headlines, titles, bodies of text
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('WiCSE Homepage'),
+        ),
+        body: Center(
+          child: Text('EHHUUUEEHHHHH :P'), 
+        ),
+      ),
+    );
+  }
+}
